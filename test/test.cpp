e9eef349857ac0fe5720260f73611591f442493e
lachlan grose <lachlan.grose@monash.edu>
2018-11-26 11:22:31 +1100

    Adding a test case in and a few more changes to get compilation on linux.

---
+++ test/test.cpp
@@ -0,0 +1,31 @@
+#include <test.h>
+#include <modelling_input.h>
+#include <continuous_property.h>
+#include <modeling_methods.h>
+#include <vector>
+using namespace Surfe;
+
+int main(int argc, char *argv[]) {
+    Basic_input input = Basic_input();
+    std::vector<Interface> interfaces;
+    std::vector<Evaluation_Point> eval;
+    double z1 = 0.0;
+    double z2 = 1.0;
+    interfaces.push_back(Interface(0.0,0.0,0.0,0.0));
+    eval.push_back(Evaluation_Point(0.1,1.1,1.1));
+    eval.push_back(Evaluation_Point(1.1,1.1,1.1));
+    interfaces.push_back(Interface(1.0,1.0,1.0,1.0));
+    //for (double x = 0.0; x < 10.0; x += 1.0) {
+    //    for (double y = 0.0; y < 10.0; y += 1.0) {
+    //        interfaces.push_back(Interface(x, y, z1, z1));
+    //        //interfaces.push_back(*b_point);
+    //    }
+    //}
+    input.itrface = &interfaces;
+    input.evaluation_pts = &eval;
+    model_parameters parameters = model_parameters();
+    Continuous_Property* metho = new Continuous_Property(parameters,input);
+    metho->run_algorithm();
+    std::cout<<"Finished"<<std::endl;
+    delete metho;
+}
---
+++ test/test.h
@@ -0,0 +1,7 @@
+#ifndef TEST_H
+#define TEST_H 
+namespace Surfe{
+    void test_01();
+    }
+
+#endif /* TEST_H */
