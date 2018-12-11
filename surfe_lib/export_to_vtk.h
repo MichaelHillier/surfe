b5257f9eb1402eb888b966b04b769602a2f35e21
lachlan grose <lachlan.grose@monash.edu>
2018-12-11 11:41:36 +1100

    surfe_lib_export for vtk writer

--- surfe_lib/export_to_vtk.h
+++ surfe_lib/export_to_vtk.h
@@ -5,6 +5,6 @@
 #include <vector>
 
 namespace Surfe {
-void write_to_vtk(Basic_input input, std::string file_name);
+void SURFE_LIB_EXPORT write_to_vtk(Basic_input input, std::string file_name);
 }
 #endif /* EXPORT_TO_VTK_H */
